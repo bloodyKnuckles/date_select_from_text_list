b033a25c279cda558206f6922531fb6ec0cc9ee3
bloodyKnuckles <treenopie@gmail.com>
2014-04-10 19:19:36 -0500

    adding files

---
+++ date_select_from_list.js
@@ -0,0 +1,71 @@
+(function () {
+
+    function myHandler(e) {
+        e = e || window.event;
+        var target = e.target || e.srcElement;
+    
+        if ( target.className.match(/\bdate_year_nav\b/) ) {
+            var current_time = new Date();
+            var current_year = current_time.getFullYear();
+            var iii = 0;
+            if ( target.className.match(/\bdate_year_nav_prev\b/) ) { iii = -1; }
+            else if ( target.className.match(/\bdate_year_nav_next\b/) ) { iii = 1; }
+            var date_select_year_children = document.getElementById('date_select_year').getElementsByTagName('a'); 
+            for ( var ii = 0, year_len = date_select_year_children.length; ii < year_len; ii++ ) {
+                var year_value = parseInt(date_select_year_children[ii].innerHTML);
+                if ( !isNaN(year_value) ) {
+                    date_select_year_children[ii].innerHTML = year_value + iii;
+                    date_select_year_children[ii].className = date_select_year_children[ii].className.replace(' date_select_year_current', '');
+                    if ( current_year == year_value + iii ) {
+                        date_select_year_children[ii].className += '  date_select_year_current';
+                    }
+                }
+            }
+        }
+
+        if ( target.className.match(/\bdate_select\b/) ) {
+            var date_select_text = document.getElementById('date_select_text');
+            var date_select_values;
+            if ( date_select_values = 
+                date_select_text.innerHTML.match(/^([A-Z][a-z]{2}\.?)? ?(\d{1,2})?(, )?(\d{4})?$/) ) {
+
+                var select_month, select_day, select_year;
+                if ( target.innerHTML.match(/^[A-Z][a-z]{2}\.?$/) ) {
+                    select_month = target.innerHTML;
+                    select_day   = ( date_select_values[2] )? date_select_values[2]: '';
+                    select_year  = ( date_select_values[4] )? date_select_values[4]: '';
+                }
+                else if ( target.innerHTML.match(/^(&nbsp;|\d)\d$/) ) {
+                    select_month = ( date_select_values[1] )? date_select_values[1]: '';
+                    select_day   = target.innerHTML.replace('&nbsp;', '');
+                    select_year  = ( date_select_values[4] )? date_select_values[4]: '';
+                }
+                else if ( target.innerHTML.match(/^\d{4}$/) ) {
+                    select_month = ( date_select_values[1] )? date_select_values[1]: '';
+                    select_day   = ( date_select_values[2] )? date_select_values[2]: '';
+                    select_year  = target.innerHTML;
+                }
+                if ( typeof select_month != 'undefined' && 
+                     typeof select_day   != 'undefined' && 
+                     typeof select_year  != 'undefined' ) {
+                    date_select_text.innerHTML = select_month + ' ' + select_day + ', ' + select_year;
+                }
+                else {
+                    date_select_text.innerHTML = 'oops try again';
+                }
+            }
+            else {
+                date_select_text.innerHTML = target.innerHTML;
+            }
+            return true;
+        }
+    }
+
+    if (document.body.addEventListener) {
+            document.body.addEventListener('click',myHandler,false);
+    }
+    else {
+        document.body.attachEvent('onclick',myHandler);
+    }
+
+}());
